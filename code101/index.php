50111808dea94937039ad609b66fe57b90f819f7
Arthur Couto <2014arthur@gmail.com>
2020-11-23 18:14:58 +0000

    Update index.php

--- code101/index.php
+++ code101/index.php
@@ -9,6 +9,16 @@
     <p>Olá Mundo</p>
     <?php
     echo ("Hoje é " . date("Y/m/d") . "</br>");
-    ?>
+    ?>  
+
+    <h1>Part II - Learnin the Language</h1>
+    <ul>
+        <li><a href="php/c1_PHPLanguageBasics.php">PHP Language Basics</a></li>
+        <li><a href="php/c2_PHPDecisionsAndLoops.php">Decisions and Loops</a></li>
+        <li><a href="php/c3_PHPFunctions.php">Functions</a></li>
+        <li><a href="php/c4_PHPArrays.php">Arrays</a></li>
+        <li><a href="php/c5_PHPSuperglobals.php">Super Globals</a></li>
+    </ul>
+
 </body>
 </html>
