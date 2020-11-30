0fcd3c2bda2db010a0ecb0c4b903499df67f2049
Arthur Couto <2014arthur@gmail.com>
2020-11-30 19:57:58 +0000

    Create funcoes.php

---
+++ code101/funcoes.php
@@ -0,0 +1,12 @@
+<?php
+
+  function areaTriangle ($base, $altura)
+  {
+    $areaTriangle = ($base * $altura) /2;
+
+    return $areaTriangle;
+  }
+  $result = areaTriangle(3, 3);
+
+  echo $result;
+?> 
